196c4931d7c67ad09f1503062eae77ace49244e5
Juergen.AMANN <juergen.amann@oecd.org>
2025-12-02 18:39:09 +0100

    Add weights support and enhance output in estim_cpd
    Introduces optional 'weights' for aggregating duplicate region-product pairs and 'weights_cpd' for weighted regression fitting. Updates output options to 'sPPP' and 'Full', improves documentation, and refines aggregation and model summary logic.

--- R/estim.R
+++ R/estim.R
@@ -29,15 +29,26 @@
 #' @param product Product identifier
 #' @param price Individual item-level price quotes; duplicated region-product
 #' pairs are aggregated by way of averaging across region-product pairs
-#' @param base.region A character specifying the base to which the estimated
+#' @param weights An optional vector of weights to be used whenever duplicate
+#' regional-product pairs are found in the data; default is `NULL`, in which
+#' case data is aggregated to region-product pairs using unweighted means. If
+#' weights are provided and duplicate regional-product pairs are found,
+#' these weights are used as part of the aggregation of average regional-product
+#' pairs; see \pkg{stats} `weighted.mean()`
+#' @param weights_cpd An optional vector of weights to be used in the fitting
+#' process of the CPD regression model; default is `NULL` and ordinary least
+#' squares is used. If non-`NULL`, weighted
+#' least squares is used, with weights \mjseqn{w} provided by `weights`, to
+#' minimise \mjseqn{\sum(w \times e^2)}; see 'Details' of  \pkg{stats} `lm()`
+#' @param base.region An optional character specifying the base to which the estimated
 #' logarithmic regional price levels are expressed
-#' When NULL, they refer to the (unweighted) regional average,
+#' When `NULL`, they refer to the (unweighted) regional average,
 #' similar to `contr.sum()`
-#' @param output Either "SPPP", which returns the estimated subnational
+#' @param output Either "sPPP", which returns the estimated subnational
 #' purchasing purchasing power parities, that is,
 #' \mjseqn{\hat{SPPP}_r = exp(\hat{\alpha}_r)} or
-#' "Std. Error", which returns the standard errors of the estimation;
-#' default is "SPPP"
+#' "Full", which summarises the key information about the estimate CPD model
+#' in a tidy `tibble` using  \pkg{broom}.
 #'
 #' @references
 #'   \insertAllCited{}
@@ -50,43 +61,78 @@
 #'   price = c(25, 28, 23, 26)
 #' )
 #'
-#' estim_cpd(df)
-#' estim_cpd(df, output = "Std. Error")
+#' estim_cpd(df, output = "sPPP")
+#' estim_cpd(df, output = "Full")
 #'
 #' @importFrom Rdpack reprompt
 #' @importFrom mathjaxr preview_rd
 #' @importFrom rlang :=
+#' @importFrom broom tidy
+#' @importFrom broom glance
+#' @importFrom stringr str_remove_all
+#' @importFrom rlang .data
+#' @importFrom rlang sym
+#' @importFrom stats mean
+#' @importFrom stats weighted.mean
+#' @importFrom stats contrasts
+#' @importFrom dplyr tally
+#' @importFrom dplyr mutate
+#' @importFrom dplyr group_by
+#' @importFrom dplyr ungroup
+#' @importFrom dplyr distinct
+#' @importFrom dplyr select
+#' @importFrom dplyr left_join
+#' @importFrom dplyr row_number
+#' @importFrom dplyr summarise
+#' @importFrom stats dummy.coef
 #' @export
 estim_cpd <- function(data,
                       region = "region",
                       product = "product",
                       price = "price",
+                      weights = NULL,
+                      weights_cpd = NULL,
                       base.region = NULL,
-                      output = "SPPP") {
+                      output = "sPPP") {
   # Checks
   ## To be implemented
   ## change regions/products to factors
   ## check that prices are numeric
   ## no empty stuff
+  if (output %not.in% c("sPPP", "Full")) stop("Field `output` incorrectly speciefied. Please choose eiterh 'sPPP', or 'Full'.")
 
-  ## Remove duplicates
+  ## Check for duplicates and aggregate
+  ### Check dimensions
   n_obs_raw <- data |> nrow()
+  n_obs <- data |>
+    group_by(.data[[region]], .data[[product]]) |>
+    tally() |>
+    nrow()
+  ### Aggregate: no weights provided
+  if (n_obs < n_obs_raw & is.null(weights)) {
   data <- data |>
-    dplyr::group_by(.data[[region]], .data[[product]]) |>
-    dplyr::summarise({{ price }} := mean(.data[[price]], na.rm = T),
+    group_by(.data[[region]], .data[[product]]) |>
+    summarise({{ price }} := mean(.data[[price]], na.rm = T),
       .groups = "drop"
     )
+  message("Duplicated region-product pairs found in data and no weights provided: Data is aggregated to region-product pairs using unweighted means.")
+  }
+  ### Aggregate: weights provided
+  if (n_obs < n_obs_raw & !is.null(weights)) {
+    data <- data |>
+      group_by(.data[[region]], .data[[product]]) |>
+      summarise({{ price }} := stats::weighted.mean(.data[[price]], w = .data[[weights]], na.rm = T),
+                       .groups = "drop"
+      )
+    message("Duplicated region-product pairs found in data and no weights provided: Data is aggregated to region-product pairs using weighted means, with weights provided in `weights`.")
+  }
 
   # Dimensions
-  n_obs <- data |> nrow()
-  if (n_obs < n_obs_raw) {
-    message("Duplicated region-product pairs found in data: Data is aggregated by averaging across region-product pairs.")
-  }
   n_region <- data |>
-    dplyr::distinct(region) |>
+    distinct(.data[[region]]) |>
     nrow()
   n_product <- data |>
-    dplyr::distinct(region) |>
+    distinct(.data[[region]]) |>
     nrow()
 
 
@@ -111,26 +157,51 @@ estim_cpd <- function(data,
 
   # Update contrasts
   if (is.null(base.region)) {
-    stats::contrasts(x = data$region) <- stats::contr.sum(levels(data$region))
-    colnames(stats::contrasts(x = data$region)) <- levels(data$region)[-nlevels(data$region)]
+    stats::contrasts(x = data[[region]]) <- stats::contr.sum(levels(data[[region]]))
+    colnames(stats::contrasts(x = data[[region]])) <- levels(data[[region]])[-nlevels(data[[region]])]
   } else {
-    stats::contrasts(x = data$region) <- stats::contr.treatment(levels(data$region))
-    colnames(stats::contrasts(x = data$region)) <- levels(data$region)[-1]
+    stats::contrasts(x = data[[region]]) <- stats::contr.treatment(levels(data[[region]]))
+    colnames(stats::contrasts(x = data[[region]])) <- levels(data[[region]])[-1]
   }
 
   # Estimate
-  est_out <- stats::lm(formula = formula, data = data, singular.ok = FALSE)
+  if (is.null(weights_cpd)) {
+    est_out <- stats::lm(formula = formula, data = data, singular.ok = FALSE)
+  } else if (!is.null(weights_cpd)) {
+    est_out <- stats::lm(formula = formula, data = data, weights = eval(rlang::sym(weights_cpd)), singular.ok = FALSE)
+  }
 
   # Output
-  if (output == "SPPP") {
+  if (output == "sPPP") {
     out <- tibble(
-      region = stats::dummy.coef(est_out)[["region"]] |> names(),
-      "SPPP" = exp(stats::dummy.coef(est_out)[["region"]])
+      region = stats::dummy.coef(est_out)[[region]] |> names(),
+      "sPPP" = exp(stats::dummy.coef(est_out)[[{{ region }}]])
     )
   }
-  if (output == "Std. Error") {
+  if (output == "Full") {
+    ## Observations by region
+    reg_nobs <- data |>
+      group_by(.data[[region]]) |>
+      tally(n = "nobs")
+
+    ## Reg model
     m <- summary(est_out)
-    out <- m$coefficients[nrow(m$coefficients), 2]
+
+    ## Regression output
+    out <- rbind(
+      broom::tidy(m) |>
+        dplyr::filter(grepl({{ region }}, term)) |>
+        dplyr::mutate(
+          term = stringr::str_remove_all(term, {{ region }}),
+          r.squared = NA, adj.r.squared = NA, sigma = NA, df = NA, df.residual = NA
+        ) |>
+        dplyr::left_join(reg_nobs, by = c("term" = {{ region }})),
+      broom::glance(m) |>
+        dplyr::mutate(
+          term = "Aggregate summary statistics",
+          estimate = NA, std.error = NA, statistic = NA, p.value = NA
+        )
+    )
   }
   return(out)
 }
