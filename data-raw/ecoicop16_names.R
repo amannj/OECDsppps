9d6064ea96765de0462ec5075f5ac8e35dac1a85
Juergen.AMANN <juergen.amann@oecd.org>
2026-02-13 15:31:25 +0100

    Map level codes to descriptive COICOP labels

--- data-raw/ecoicop16_names.R
+++ data-raw/ecoicop16_names.R
@@ -1,3 +1,11 @@
 ecoicop16_names <- readr::read_rds("V:/SUBNATIONAL_PRICES/sandbox/_General/ecoicop16_names.rds")
 
+ecoicop16_names <- ecoicop16_names |>
+  dplyr::mutate(level = dplyr::case_when(level == 1 ~ "Division  (COICOP2)",
+    level == 2 ~ "Group (COICOP3)",
+    level == 3 ~ "Class (COICOP4)",
+    level == 4 ~ "Subclass  (COICOP5)",
+    .default = NA
+  ))
+
 usethis::use_data(ecoicop16_names, overwrite = TRUE)
